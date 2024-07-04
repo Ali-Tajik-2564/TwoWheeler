bc208560893bb0bacbf366d954bb2eceb163de38
Ali <ali.tajik.1385.a@gmail.com>
2024-07-04 20:06:14 +0330

    not found page and contact us done

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,59 @@
 import React from 'react';
-
+import { IoMdMail } from 'react-icons/io';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { IoLocationSharp } from 'react-icons/io5';
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="w-full h-auto flex md:flex-row flex-col-reverse p-0 ">
+      <img
+        src="./Img/map.png"
+        alt=""
+        className="md:w-1/2 w-full  md:h-auto h-fit bg-contain"
+      />
+      <div className="md:w-1/2 w-full h-auto bg-white px-20 py-12 flex flex-col items-start justify-between space-y-5">
+        <p className="text-3xl md-xl font-extrabold text-bgPrimary">
+          Contact Us
+        </p>
+        <div className="w-full h-auto flex flex-col items-start justify-between md:space-y-6 space-y-4">
+          <div className="flex items-center space-x-6">
+            <IoLocationSharp className="text-rose-400 w-8 h-8 " />
+            <p className="md:text-lg text-base font-normal text-bgPrimary/70 w-60">
+              2005 Broken Dream Blvd Daytona Beach, FL 32122
+            </p>
+          </div>
+          <div className="flex items-center space-x-6">
+            <FaPhoneAlt className="text-green-400 w-8 h-8 " />
+            <p className="md:text-lg text-base font-normal text-bgPrimary/70">
+              (343)-7509-2453
+            </p>
+          </div>
+          <div className="flex items-center space-x-6">
+            <IoMdMail className="text-blue-400  w-8  h-8 " />
+            <p className="md:text-lg text-base font-normal text-bgPrimary/70">
+              twowheelers@xmail.com
+            </p>
+          </div>
+        </div>
+        <div className="w-full h-auto flex flex-col items-start justify-between md:space-y-5 space-y-3">
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="w-full p-2 text-lg font-medium text-bgPrimary/50 shadow-md border-2 border-bgPrimary/20   rounded-md outline-none ring-0 focus:ring-0 focus:outline-none"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-full p-2 text-lg font-medium text-bgPrimary/50 shadow-md border-2 border-bgPrimary/20   rounded-md outline-none ring-0 focus:ring-0 focus:outline-none"
+          />
+          <textarea
+            placeholder="Message"
+            className="w-full h-fit p-2 text-lg font-medium text-bgPrimary/50 shadow-md border-2 border-bgPrimary/20   rounded-md outline-none ring-0 focus:ring-0 focus:outline-none"
+          />
+        </div>
+        <button className="mr-auto w-36  p-1 rounded-md bg-textPrimary hover:bg-textPrimary/90 text-bgPrimary font-semibold text-lg ">
+          Send
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="w-full h-auto flex md:flex-row flex-col items-center justify-between m-0   bg-gradient-to-r from-white from-50% ">
+      <div className="md:w-2/5  w-full h-full flex flex-col justify-between items-center text-bgPrimary  lg:px-20 px-8 lg:space-y-24 space-y-12 py-5 md:bg-transparent ">
+        <div className="flex flex-col items-start justify-between gap-1 mr-auto">
+          <p className="lg:text-3xl text-2xl font-extrabold">Oops...</p>
+          <p className="lg:text-xl text-lg font-medium">Page not found</p>
+        </div>
+        <p className="lg:text-lg text-md font-normal">
+          Unfortunately, the page you’re looking for is either under
+          maintenance, unaccessible, or there’s a server problem in our site.
+          Please go back to home page.
+        </p>
+        <Link to="./">
+          <button
+            className="lg:text-xl text-lg font-bold  border-b border-bgPrimary p-1
+      ">
+            Go back to Home
+          </button>
+        </Link>
+      </div>
+      <img
+        src="./Img/image-23.png "
+        alt="landing-page-img"
+        className="md:w-3/5 w-full md:h-screen h-fit m-0  md:-z-10  bg-contain md:opacity-1 opacity-85
+      "
+      />
+    </div>
+  );
 }
