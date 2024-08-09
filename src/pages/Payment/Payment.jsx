1c2364e1d8d1356bce27ecbf6186497abdcce213
Ali <ali.tajik.1385.a@gmail.com>
2024-08-09 19:29:43 +0330

    Update Payment.jsx

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -4,20 +4,21 @@ import Dropdown from 'react-bootstrap/Dropdown';
 export default function Payment() {
   const [bankName, setBankName] = useState();
   console.log(bankName);
+  const SubmitOrder = () => {};
   return (
-    <div className="w-full h-auto flex item-center justify-between p-12 bg-white">
-      <div className="w-1/2 h-full flex flex-col items-start justify-between p-4 space-y-12">
+    <div className="w-full h-auto flex flex-col-reverse lg:flex-row item-center justify-between lg:p-14 p-3 bg-white lg:space-y-0 space-y-5 lg:space-x-10">
+      <div className="lg:w-2/5 md:w-4/5 w-full h-full flex flex-col items-start justify-between p-4 space-y-12">
         {/* payment method  */}
         <div className="flex w-full flex-col items-start space-y-3">
           <p className="text-2xl font-medium ">Payment Method</p>
-          <div className="w-full h-auto flex justify-evenly items-center">
-            <div className="w-full h-auto flex space-x-2 items-center ">
+          <div className="w-full h-auto flex flex-row   justify-evenly items-start">
+            <div className="w-auto h-auto flex space-x-2 items-center ">
               <label htmlFor="input" className="text-base font-light">
                 Credit Cart
               </label>
-              <input type="radio" name="Payment-info" />
+              <input type="radio" name="Payment-info" checked="true" />
             </div>
-            <div className="w-full h-auto flex space-x-2 items-end ">
+            <div className="w-auto h-auto flex space-x-2 items-end ">
               <img
                 src="../../Img/Apple_Card.svg.png"
                 alt=""
@@ -27,7 +28,7 @@ export default function Payment() {
 
               <input type="radio" name="Payment-info" />
             </div>
-            <div className="w-full h-auto flex space-x-2 items-end ">
+            <div className="w-auto h-auto flex space-x-2 items-end ">
               <img
                 src="../../Img/PayPal.svg.png"
                 alt=""
@@ -47,9 +48,9 @@ export default function Payment() {
           <input
             type="text"
             placeholder="Name "
-            className="focus:ring-0 focus:outline-none w-3/4 text-bgPrimary font-medium text-lg border-b-2 border-bgPrimary/30  bg-none"
+            className="focus:ring-0 focus:outline-none md:w-4/5 w-full text-bgPrimary font-medium text-lg border-b-2 border-bgPrimary/30  bg-none"
           />
-          <div className="flex w-full h-auto items-center justify-start">
+          <div className="flex  md:w-4/5 w-full h-auto items-end justify-start border-b-2 border-bgPrimary/30 p-2">
             <Dropdown>
               <Dropdown.Toggle
                 id="dropdown-basic"
@@ -58,7 +59,7 @@ export default function Payment() {
                 {bankName ? (
                   <img src={bankName} alt="" className="h-8" />
                 ) : (
-                  'Select Bank Name'
+                  'Bank Name'
                 )}
               </Dropdown.Toggle>
 
@@ -83,29 +84,76 @@ export default function Payment() {
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
+            <input
+              type="text"
+              placeholder="Number "
+              className="focus:ring-0 focus:outline-none w-full text-bgPrimary font-medium text-lg border-l border-bgPrimary/50  px-2  bg-none"
+            />
+          </div>
+          <div className="flex w-full h-auto  items-start justify-between   ">
+            <div className="flex flex-col items-start justify-start space-y-6 w-1/2">
+              <p className="text-bgPrimary/70 font-medium text-xl">
+                Expiration
+              </p>
+              <input
+                type="date"
+                name="Expiration Date"
+                className="focus:ring-0 focus:outline-none  text-bgPrimary/70 font-medium text-lg border-b-2 border-bgPrimary/30  bg-none w-auto"
+              />
+            </div>
+
+            <div className="flex flex-col items-start justify-start space-y-6 w-1/2">
+              <p className="text-bgPrimary/70 font-medium text-xl">CVV</p>
+              <input
+                type="text"
+                name="CVV"
+                placeholder="nnn"
+                className="focus:ring-0 focus:outline-none w-3/4 text-bgPrimary/70 font-medium text-lg border-b-2 border-bgPrimary/30  bg-none"
+              />
+            </div>
           </div>
         </div>
         {/* payment details  */}
+        {/* payment RUle /  */}
+        <p className="text-bgPrimary font-medium text-base line-clamp-2">
+          By clicking “Confirm Payment”, you agree to our store regulations.
+        </p>
+        {/* payment RUle /  */}
+        {/* payment buttons */}
+        <div className="flex flex-row   items-start justify-between   w-full h-auto ">
+          <button
+            onClick={SubmitOrder}
+            className=" md:w-48 w-auto px-4 p-2 rounded-md bg-textPrimary hover:bg-textPrimary/90 text-bgPrimary font-semibold text-base  text-center">
+            Confirm Payment
+          </button>
+          <button
+            className=" md:w-48 w-auto px-4  p-2  text-center rounded-md border-1 border-textPrimary hover:bg-textPrimary/90 text-bgPrimary font-Bold text-base duration-150 "
+            onClick={() => history.go(-1)}>
+            Cancel
+          </button>
+        </div>
+        {/* payment buttons */}
       </div>
 
       {/* item info  */}
-      <div className="w-96 h-auto relative rounded-2xl overflow-hidden Payment-Box-Shadow">
+      <div className="lg:w-2/5 w-96  h-auto relative rounded-2xl overflow-hidden Payment-Box-Shadow  max-lg:mx-auto">
         <img
           src="../../Img/unsplash_7GeprSfqLQ.png"
           alt=""
           className="w-full h-auto bg-cover
-        rounded-lg -z-20"
+        rounded-lg -z-20 "
         />
-        <div className="absolute  bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-slate-800  z-20 p-3">
-          <div className="flex flex-col items-end justify-between w-full h-auto space-y-3"></div>
-          <p className="text-white font-bold text-xl ">Your Order : </p>
-          <p className="text-white font-bold text-xl ">
-            Ducati 1199 Panigale (2012)
-          </p>
-          <p className="text-white font-bold text-lg">
-            color : vivid white and black{' '}
-          </p>
-          <p className="text-textPrimary font-bold text-2xl">$12,999</p>
+        <div className="absolute  bottom-0 left-0 w-full h-auto bg-gradient-to-t from-slate-800  z-20 p-3">
+          <div className="flex flex-col items-start justify-start w-full h-auto space-y-4">
+            <p className="text-white font-bold text-xl ">Your Order : </p>
+            <p className="text-white font-bold text-xl ">
+              Ducati 1199 Panigale (2012)
+            </p>
+            <p className="text-white font-bold text-lg">
+              color : vivid white and black{' '}
+            </p>
+            <p className="text-textPrimary font-bold text-2xl">$12,999</p>
+          </div>
         </div>
         {/* item info  */}
       </div>
