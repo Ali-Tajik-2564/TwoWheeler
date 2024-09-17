7aedadc3cd64c3a6aab1585dfcf2921afb3de320
Ali <ali.tajik.1385.a@gmail.com>
2024-09-17 18:40:19 +0330

    user orders table done
    next move : deploy

--- src/pages/orderHistory/orderHistory.jsx
+++ src/pages/orderHistory/orderHistory.jsx
@@ -14,31 +14,36 @@ export default function OrderHistory() {
   console.log(ordersData);
 
   return (
-    <div className="w-full h-screen p-4 flex justify-center items-center  rounded-md overflow-hidden">
-      <table className="w-8/12 h-auto m-2  rounded-md ">
-        <thead className="bg-slate-500 text-bgPrimary text-xl font-medium border-b border-slate-600 w-auto h-auto overflow-hidden p-2">
-          <tr className="w-full h-auto   p-4">
-            <th className="m-2"> Product Id</th>
-            <th>Date</th>
-            <th>Price</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {isSuccess && (
-            <>
-              {ordersData?.map((order) => (
-                <tr className="w-full h-auto border-b-2 border-bgPrimary text-lg ">
-                  <td>{order.productID}</td>
-                  <td>{order.date}</td>
-                  <td>{order.price}</td>
-                  <td>{order.status}</td>
-                </tr>
-              ))}
-            </>
-          )}
-        </tbody>
-      </table>
+    <div className="w-full min-h-screen p-8 bg-bgPrimary/60">
+      <div className="text-xl font-semibold text-textPrimary  w-full border-b border-bgPrimary/70 pb-3">
+        <h1 className="text-xl font-semibold text-bgPrimary  w-full border-b border-bgPrimary/70 pb-3">
+          Your Orders
+        </h1>
+        <table className="w-full my-4 ">
+          <thead>
+            <tr className="p-3 md:text-base text-sm font-medium text-textPrimary  flex justify-between items-center flex-row-reverse border-b-2 border-bgPrimary/70 mb-3 gap-x-1">
+              <th> Product Id</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isSuccess && (
+              <>
+                {ordersData?.map((order) => (
+                  <tr className="p-2 text-sm md:text-sm font-light text-bgPrimary  flex justify-between items-center flex-row-reverse gap-x-4">
+                    <td>{order.productID}</td>
+                    <td>{order.date}</td>
+                    <td>{order.price}</td>
+                    <td>{order.status}</td>
+                  </tr>
+                ))}
+              </>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
